39131c9e92dd3d423712c2854f298ef67a9708fb
yukeshwarp <Yukeshwarp@docu3c.com>
2025-01-18 13:45:59 +0530

    Add files via upload

---
+++ utils/config.py
@@ -0,0 +1,11 @@
+import os
+
+
+llm_api_key = os.getenv("AZURE_API_KEY")
+azure_llm_endpoint = os.getenv("AZURE_ENDPOINT")
+llm_model = os.getenv("LLM_MODEL")
+llm_api_version = "2024-10-01-preview"
+
+redis_host = os.getenv("REDIS_HOST")
+redis_port = 6379
+redis_password = os.getenv("REDIS_KEY")
