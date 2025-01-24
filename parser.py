a4b1aacb0c36916ce52c9f5c257e62cd944916fa
yukeshwarp <Yukeshwarp@docu3c.com>
2025-01-24 19:57:33 +0530

    Update parser.py

--- parser.py
+++ parser.py
@@ -197,6 +197,7 @@ def extractor(doc):
         1. Trademark Name Extraction:
            - The trademark name is located immediately before the words "Registered," "Published," or "Cancelled."  
            - Extract the trademark name exactly as it appears without alterations.  
+           - Extract all trademark entries irrespective of their classification and status.
         
         2. Page Number Extraction:
            - The start page number is the last number in each entry.  
