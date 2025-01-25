be226d8ef2330c3a029853d1f813469d6fa3c2ff
yukeshwarp <Yukeshwarp@docu3c.com>
2025-01-25 22:50:50 +0530

    Update parser.py

--- parser.py
+++ parser.py
@@ -34,7 +34,7 @@ def extract_search_target(doc):
                 "mark_searched", "target_class", "goods_&_services"
                 
                 Do not include any additional text or explanations.  
-                Determine target_class based on the international trademark classes that the goods_&_services comes under.
+                Determine target_class based on the international trademark classes that the goods_&_services comes under, Don't consider Classes Searched as target_class.
 
                 Document chunk to extract from: 
                 {chunk}
